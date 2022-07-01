ef22203b1e9fd6383f20834772a469aed963741e
YuliyaMinsk <yuliya.rostova@gmail.com>
2022-07-02 01:30:49 +0300

    fix: update attribute block to Product page

--- src/components/sections/InfoProduct.jsx
+++ src/components/sections/InfoProduct.jsx
@@ -8,12 +8,17 @@ class InfoProduct extends Component {
         <h1 className="product-brand">{this.props.info.brand}</h1>
         <h2 className="product-title">{this.props.info.name}</h2>
         {this.props.info.attributes.map(({ name, type, items }) => (
-          <div className={`product-attributes product-${name}`}>
+          <div className={`product-attributes product-${name.toLowerCase()}`}>
             <p className="product-subtitle">{name}:</p>
             <div>
               {items.map(({ displayValue, value }) => (
                 <span className={`product-attribute ${type}`} value={value}>
-                  <input type="radio" name={`attribute-${name}`} id={displayValue} value={value} />
+                  <input
+                    type="radio"
+                    name={`attribute-${name.toLowerCase()}`}
+                    id={displayValue}
+                    value={value}
+                  />
                   <label for={displayValue} style={{ backgroundColor: value }}>
                     <span class={displayValue}>{displayValue}</span>
                   </label>
