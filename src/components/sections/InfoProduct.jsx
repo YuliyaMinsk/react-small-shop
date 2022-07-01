f3d432b8c86b69bde1443c5b80059de4809eb8ed
YuliyaMinsk <yuliya.rostova@gmail.com>
2022-07-01 20:54:12 +0300

    feat: add attribute block to Product page

--- src/components/sections/InfoProduct.jsx
+++ src/components/sections/InfoProduct.jsx
@@ -4,22 +4,30 @@ class InfoProduct extends Component {
   render() {
     // console.log('Product info: ', this.props.info);
     return (
-      <div className="product-gallery">
-        <div className="product-brand">
-          <h1>{this.props.info.brand}</h1>
-        </div>
-        <div className="product-title">
-          <h2>{this.props.info.name}</h2>
-        </div>
-        <div className="product-size">{this.props.info.brand}</div>
-        <div className="product-color">{this.props.info.brand}</div>
-        <div className="product-price">{this.props.info.brand}</div>
+      <div className="product-info">
+        <h1 className="product-brand">{this.props.info.brand}</h1>
+        <h2 className="product-title">{this.props.info.name}</h2>
+        {this.props.info.attributes.map(({ name, type, items }) => (
+          <div className={`product-attributes product-${name}`}>
+            <p className="product-subtitle">{name}:</p>
+            <div>
+              {items.map(({ displayValue, value }) => (
+                <span className={`product-attribute ${type}`} value={value}>
+                  <input type="radio" name={`attribute-${name}`} id={displayValue} value={value} />
+                  <label for={displayValue} style={{ backgroundColor: value }}>
+                    <span class={displayValue}>{displayValue}</span>
+                  </label>
+                </span>
+              ))}
+            </div>
+          </div>
+        ))}
         <div>
           <button className="button button-colored button-big product-button">Add to cart</button>
         </div>
         <div
           className="product-description"
-          dangerouslySetInnerHTML={{ __html: this.props.info.description }}
+          dangerouslySetInnerHTML={{ __html: this.props.info.description.replaceAll('h1>', 'h3>') }}
         />
       </div>
     );
