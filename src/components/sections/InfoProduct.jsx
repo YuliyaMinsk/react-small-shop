539e75bee1b37ae2dee5e01273c44bab0cf39d45
YuliyaMinsk <yuliya.rostova@gmail.com>
2022-07-02 11:03:52 +0300

    fix: update case with attributes with spaces

--- src/components/sections/InfoProduct.jsx
+++ src/components/sections/InfoProduct.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+function updateAttribute(attribute) {
+  return attribute.toLowerCase().split(' ').join('-');
+}
+
 class InfoProduct extends Component {
   render() {
     // console.log('Product info: ', this.props.info);
@@ -8,19 +12,22 @@ class InfoProduct extends Component {
         <h1 className="product-brand">{this.props.info.brand}</h1>
         <h2 className="product-title">{this.props.info.name}</h2>
         {this.props.info.attributes.map(({ name, type, items }) => (
-          <div className={`product-attributes product-${name.toLowerCase()}`}>
+          <div className={`product-attributes product-${updateAttribute(name)}`}>
             <p className="product-subtitle">{name}:</p>
             <div>
               {items.map(({ displayValue, value }) => (
                 <span className={`product-attribute ${type}`} value={value}>
                   <input
                     type="radio"
-                    name={`attribute-${name.toLowerCase()}`}
-                    id={displayValue}
+                    name={`attribute-${updateAttribute(name)}`}
+                    id={updateAttribute(name.concat(' ', value))}
                     value={value}
                   />
-                  <label for={displayValue} style={{ backgroundColor: value }}>
-                    <span class={displayValue}>{displayValue}</span>
+                  <label
+                    for={updateAttribute(name.concat(' ', value))}
+                    style={{ backgroundColor: value }}
+                  >
+                    <span class={updateAttribute(name.concat(' ', value))}>{displayValue}</span>
                   </label>
                 </span>
               ))}
